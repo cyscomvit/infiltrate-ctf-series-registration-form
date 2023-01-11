d5435451dd041fda4a95e72ce57afc80e2994bdd
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2023-01-11 09:07:04 +0530

    port=80

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    app.run(debug=bool(os.getenv("DEBUG")), host="0.0.0.0", port=5000)
+    app.run(debug=bool(os.getenv("DEBUG")), host="0.0.0.0", port=80)
